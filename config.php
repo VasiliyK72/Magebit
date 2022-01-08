4379449ff0f95825456ca829f09ef75627d03950
VasiliyK72 <akvaferma@gmail.com>
2022-01-08 23:56:56 +0200

    1

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
 
-if(ERROR_REPORTING) {
-    ini_set('error_reporting', E_ALL);
-    ini_set('display_errors', 1);
-    
-    ini_set('display_startup_errors', 1);
-}
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 
 // DB
 define('PDO_DSN', 'mysql:host=localhost;dbname=magebit');
